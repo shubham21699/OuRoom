a85696f0f57146ffe37abea5ebb4303950be8c48
shubham21699 <shubham21699@gmail.com>
2021-09-30 16:47:27 +0530

    chat.js modified

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,19 +2,20 @@ import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
 import io from "socket.io-client";
 import Fade from 'react-reveal/Fade';
+import './Chat.css';
 
 import TextContainer from '../TextContainer/TextContainer';
 import Messages from '../Messages/Messages';
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
 
-import './Chat.css';
 
 const ENDPOINT = 'http://localhost:5000/';
 
 let socket;
 
 const Chat = ({ location }) => {
+
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
   const [users, setUsers] = useState('');

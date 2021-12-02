ab91ac0f4b4ab42b8312025db7ac19571a1f8b0b
Gautam Diwan <gautamdiwan3@gmail.com>
2021-12-02 13:54:36 +0530

    Added Video Player functionality

--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -6,8 +6,10 @@ import './TextContainer.css';
 
 const TextContainer = ({ users }) => (
   <div className="textContainer">
-    <div>
     <h2>Realtime Communication Without any Privacy issues <img className="chat-logo" src={chatLogo} alt="Logo" />  </h2> 
+    <div className="video-container">
+    <h2>Add a Youtube video link to get started!</h2>
+    {/* <iframe className="videoPlayer" src="https://www.youtube.com/embed/pQN-pnXPaVg" width="350" height="250" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
       {/* <h2>Realtime Communication Without any Privacy issues <span role="img" aria-label="emoji">💬</span></h2> */}
       {/* <h2>with  <span role="img" aria-label="emoji">❤️</span></h2> */}
       {/* <h2>Try it out right now! <span role="img" aria-label="emoji">⬅️</span></h2> */}
@@ -26,6 +28,24 @@ const TextContainer = ({ users }) => (
                   </div>
                 ))}
               </h3>
+              <form className="videoForm">
+                <input className="videoLink" type="text" placeholder="Enter the video link"></input>
+                <button className="videoUpload" onClick={(event) => { 
+                  event.preventDefault();
+                  let link=document.querySelector('.videoLink')
+                  let video = document.querySelector('.video-container')
+                  let eqIndex=link.value.indexOf('=')
+                  let code=""
+                  if (eqIndex > -1){
+                    code=link.value.substring(eqIndex+1)
+                  } else {
+                    code=link.value.substring((link.value.lastIndexOf('/'))+1)
+                  }
+                  let ytlink = `https://www.youtube.com/embed/${code}`
+                  console.log(ytlink)
+                  video.innerHTML=`<iframe className="videoPlayer" src=${ytlink} width="350" height="250" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+                }}>UPLOAD LINK</button>
+              </form>
             </div>
           </div>
         )
